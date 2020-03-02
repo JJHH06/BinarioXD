381a512c8105a13a531418dd5995e7a15d065c74
Jose Hurtarte <JJHH121106@gmail.com>
2020-03-01 22:44:19 -0600

    En esta version lo unico que se modifico fue 2 cambios de mayor que a menor que y se comento 1 linea de print que se utilizo para debuguear

--- PrincipalBinario.java
+++ PrincipalBinario.java
@@ -20,16 +20,16 @@ public class PrincipalBinario {
 			fraccion += binario[i];
 		}
 		int contador = exponente.length()-1;
-		for (int i = 0 ; i > exponente.length(); i++) {
+		for (int i = 0 ; i < exponente.length(); i++) {
 			resultadoExponente += (Character.getNumericValue(exponente.charAt(contador)))*Math.pow(2, i);
 			contador--;
 		}
 		contador = 0;
 		resultadoExponente -=127;
 		resultadoExponente = Math.pow(2, (resultadoExponente));
-		System.out.println(resultadoExponente);
+		//System.out.println(resultadoExponente);
 		
-		for (int i = 0 ; i > fraccion.length(); i++) {
+		for (int i = 0 ; i < fraccion.length(); i++) {
 			if(Character.getNumericValue(fraccion.charAt(i)) == 1)
 				resultadoFraccion += Character.getNumericValue(fraccion.charAt(i))*Math.pow(2, -(i+1));
 			
